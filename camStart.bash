9c228ed58911baec4ba57720f36435a7d5569607
CircleDev <circledev9@gmail.com>
2025-02-02 07:27:00 +0530

    Changed camStart.bash to use sudo in a proper way, also chmod +x to micStart.bash

--- camStart.bash
+++ camStart.bash
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo Sudo Password: 
-read -s pass
-echo $pass | sudo -S modprobe -r v4l2loopback
-echo $pass | sudo -S modprobe v4l2loopback devices=1  exclusive_caps=1 video_nr=9 card_label="v4l2loopback"
+sudo modprobe -r v4l2loopback
+sudo modprobe v4l2loopback video_nr=9,10,11 card_label="v4l2loopback","looking","down"
 python3 cam.py
 
